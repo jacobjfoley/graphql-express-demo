884104226a27de89a08c29530c9e5c67cdaee9bb
Jacob Foley <jacob.j.foley@gmail.com>
2020-04-12 09:26:20 +1000

    Add documentation

--- src/middleware/graphql/types.js
+++ src/middleware/graphql/types.js
@@ -1,11 +1,13 @@
 // Define our types.
 module.exports = `
+  """An author of books."""
   type Author {
     id: ID!
     name: String!
     books: [Book]!
   }
 
+  """A book written by an author."""
   type Book {
     id: ID!
     name: String!
